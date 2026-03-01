fda87238e347f3b891dcf64190c691bcc54d2bef
Rafael Neves <rafaelnvss002@gmail.com>
2026-03-01 11:34:39 -0300

    fix

--- sisu-dashboard/app/(pages)/cursos/[curso]/page.tsx
+++ sisu-dashboard/app/(pages)/cursos/[curso]/page.tsx
@@ -1,9 +1,8 @@
 
-import CursoClientPage from "./CursoClientPage";
-
 export default async function CursoPage({ params }) {
+  console.log(params)
   const { curso } = await params;
   const cursoDecoded = decodeURIComponent(curso);
 
-  return <CursoClientPage curso={cursoDecoded} />;
+  return null
 }
