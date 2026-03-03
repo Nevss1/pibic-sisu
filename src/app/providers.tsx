252d843a07e367022f7648a2793df93c5e4315d1
Rafael Neves <rafaelnvss002@gmail.com>
2026-03-03 15:36:25 -0300

    fix Provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,7 +4,17 @@ import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5,
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
